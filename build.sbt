2010897e78a41a0db864cbdddba9c64f60e81ce2
chandramgc <chandamgc@gmail.com>
2018-11-25 09:50:29 -0500

    inital commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "spark-scala-snippet"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
---
+++ src/main/scala/intro.scala
@@ -0,0 +1,5 @@
+class intro {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world")
+  }
+}
